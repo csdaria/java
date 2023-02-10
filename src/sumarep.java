ed8f82ae43764e99fc57ded8fca4fafa5d61b29a
csdaria <cs.daria@proton.me>
2023-02-10 19:36:15 +0000

    first try

---
+++ src/sumarep.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class sumarep {
+    public static void main(String[] args) {
+        int i = 1; int n=0; int suma=0;
+        while (i <= 100) {
+            suma += i;
+            i = i + 2;
+            System.out.println(suma);
+
+        }
+        while (n <= 100) {
+            suma +=n;
+            n = n + 2;
+            System.out.println(suma);
+            }
+        }
+    }
+
